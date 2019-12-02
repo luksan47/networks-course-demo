1cb27a6b7d342f45406582571bb64823c9dcae71
teglaservin <teglaservin96@gmail.com>
2019-12-02 11:00:00 +0100

    logs

--- majv.js
+++ majv.js
@@ -15,14 +15,15 @@ function majv(cy, sourceNode, targetNode, addedEles) {
     var server_of_u = u.parent();
     var server_of_v = v.parent();
     var size_of_u = u.component().nodes().length;
-    var size_of_v = v.component().nodes().length - 1;
+    var size_of_v = v.component().nodes().length;
     if (server_of_u !== server_of_v) {
         move(cy, u.component().nodes(), server_of_v);
     }
     cy.add(addedEles);
     var i = 1;
+    console.log("|Cu| = ",size_of_u);
+    console.log("|Cv| = ",size_of_v);
     while (Math.pow(2,i) < (size_of_u + size_of_v)) {
-        console.log(Math.pow(2,i));
         if ((Math.pow(2,i) > size_of_u) && (Math.pow(2,i) > size_of_v)) {
             majority_vote(u.component().nodes());
             break;
@@ -41,13 +42,18 @@ function majority_vote(cu) {
         if (cu[i].data('colorid') == 'server_0') {
             if (first_yellow == -1) first_yellow = i;
             yellow++;
-        } else {
+        } else if (cu[i].data('colorid') == 'server_1') {
             if (first_black == -1) first_black = i;
             black++;
         }
     }
     console.log(yellow);
     console.log(black);
+    console.log(cu[0]);
+    var cu0 = cu[0];
+    console.log(cu0.parent().data('id'));
+    console.log(cu[0].parent().data('id'));
+    console.log(cu[0].data('parent'));
     if ( (cu[0].data('parent') == 'server_0') && (black > yellow) ) {
         move(cy, cu, cu[first_black].data('colorid'));
     } else if ( (cu[0].data('parent') == 'server_1') && (yellow > black) ) {
